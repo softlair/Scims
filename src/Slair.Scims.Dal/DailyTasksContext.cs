2a395e97c8b54d3b1b7f45973f3098958ea306a2
arrian0511 <a_pascval@yahoo.com>
2016-08-21 17:51:11 +0800

    Modified Entity Based implementation of table, and added Context for Plan and DailyTasks

---
+++ src/Slair.Scims.Dal/DailyTasksContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Slair.Scims.Model;
+using Slair.Scims.Model.Abstractions;
+
+namespace Slair.Scims.Dal
+{
+	public class DailyTasksContext : DbContext
+	{
+		public DbSet<DailyTasksRecord>		Tasks		{ get; set; }
+
+		public DailyTasksContext(DbContextOptions<AdministrationContext> options) : base (options)
+		{
+		}
+
+		protected override void OnModelCreating (ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<DailyTasksRecord> ( )
+				.HasMany (t => t.Tasks);
+		}
+	}
+}
